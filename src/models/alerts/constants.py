5878db3d57f024dd2af80cdce7ee1815e0ac8a61
heftybr0k3r <oembuilder11@hotmail.com>
2018-10-13 17:35:38 +0200

    modified:   src/models/alerts/constants.py

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,5 +1,7 @@
-URL = "https://api.mailgun.net/v3/sandboxbee11ac7b5124bfc8482c91592000b78.mailgun.org/messages"
-API_KEY = "92151350fe84d73d9ae8beae671f7c6c-c8e745ec-ac7a46ec"
-FROM = "Price Alert Service <pricealert@pricealert.uu.me>"
+import os
+
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
